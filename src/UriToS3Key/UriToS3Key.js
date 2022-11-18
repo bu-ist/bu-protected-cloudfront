4cee2e3b86f417700379ac067d851f1d0923c808
Jonathan Williams <tamlin@mac.com>
2022-11-17 19:59:17 -0500

    :sparkles: add initial access checking
    compares a username from the request header to the rules loaded from a dynamodb table based on the group name in url

--- src/UriToS3Key/UriToS3Key.js
+++ src/UriToS3Key/UriToS3Key.js
@@ -1,14 +1,21 @@
 'use strict'
 
+const { DynamoDB } = require('aws-sdk');
 const { parse } = require('querystring')
 
 const DEFAULT_EXTENSION = 'webp'
 const BAD_JPG_EXTENSION = 'jpg'
 const GOOD_JPG_EXTENSION = 'jpeg'
 
-const UriToS3Key = event => {
+const UriToS3Key = async event => {
   const { request, request: { headers, querystring, uri } } = event.Records[0].cf
   const { h: height = '', w: width } = parse(querystring)
+
+  // Apply access control.
+  const allowed = await checkPermission(uri, headers);
+  if (!allowed) {
+    return deniedResponse;
+  }
 
   if (!width || isNaN(parseInt(width, 10))) return request
 
@@ -36,5 +43,55 @@ const UriToS3Key = event => {
 
   return request
 }
+
+async function checkPermission(uri, headers) {
+  const dynamoDb = new DynamoDB.DocumentClient();
+
+  if  (!uri.includes('__restricted')) {
+    // Unrestricted items are always allowed.
+    return true;
+  }
+
+  // Get the username from the headers.
+  let userName = '';
+  if ('x-bu-shib-username' in headers) {
+    userName = headers['x-bu-shib-username'][0].value;
+  }
+
+  //const allowed = await checkPermissionFromAuthServer(uri, headers);
+
+  // Get the group name from the uri, it is the segment after the "/__restricted/" segment.
+  const pathSegments = uri.split('/');
+  const groupName = pathSegments[pathSegments.indexOf('__restricted') + 1];
+
+
+  // Get the associated group rules from DynamoDB.
+  const { Item } = await dynamoDb.get({
+    TableName: 'image-flex-079560432659-dev-AccessControl', 
+    Key: { SiteAndGroupKey: groupName } }
+  ).promise();
+
+  // Parse the rules.
+  const { users } = JSON.parse(Item.rules);
+
+  // Apply the rules.
+  const allowed = users.includes(userName);
+
+  return allowed;
+}
+
+// A static response to return for denied requests.
+const deniedResponse = {
+  body: 'Access denied',
+  bodyEncoding: 'text',
+  headers: {
+      'cache-control': [{
+          key: 'Cache-Control',
+          value: 'no-cache'
+       }],
+  },
+  status: '401',
+  statusDescription: 'Unauthorized'
+};
 
 module.exports = UriToS3Key
